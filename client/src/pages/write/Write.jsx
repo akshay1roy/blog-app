66355640110a0a17170d7f0f5df3cfb777280969
Akshay kumar <akshaykumar880466@gmail.com>
2024-10-23 01:01:27 +0530

    init

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -7,6 +7,7 @@ function Write() {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
   const [file, setFile] = useState(null);
+  const [category, setCategory] = useState("");
   const { user } = useContext(Context);
 
   const handleSubmit = async (e) => {
@@ -15,6 +16,7 @@ function Write() {
       username: user.username,
       title,
       desc,
+      categories: category,
     };
     if (file) {
       const data = new FormData();
@@ -25,14 +27,12 @@ function Write() {
 
       try {
         await axios.post("/upload", data);
-      } catch (error) {
-
-      }
+      } catch (error) {}
     }
 
     try {
       const res = await axios.post("/posts", newPost);
-      // console.log(res)
+      console.log(res)
       window.location.replace("/post/" + res.data._id);
     } catch (error) {}
   };
@@ -43,7 +43,37 @@ function Write() {
       )}
 
       <form className="writeForm" onSubmit={handleSubmit}>
-        <div className="writeFormGroup" >
+      <div className="writeFormGroup" style={{ display: "flex", gap: "10px" }}>
+          <label htmlFor="fileInput">
+            <i className="writeIcon fa-solid fa-plus"></i>
+          </label>
+          <input
+            type="file"
+            id="fileInput"
+            style={{ display: "none" }}
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+
+          {/* Title Input */}
+          <input
+            type="text"
+            placeholder="Title"
+            className="writeInput"
+            autoFocus={true}
+            onChange={(e) => setTitle(e.target.value)}
+            style={{ flex: 2 }}
+          />
+
+          {/* Category Input */}
+          <input
+            type="text"
+            placeholder="Category music, dance etc"
+            className="writeInput writeInputCategory"
+            onChange={(e) => setCategory(e.target.value)}
+            style={{ flex: 1 }} 
+          />
+        </div>
+        {/* <div className="writeFormGroup">
           <label htmlFor="fileInput">
             <i className=" writeIcon fa-solid fa-plus"></i>
           </label>
@@ -60,7 +90,7 @@ function Write() {
             autoFocus={true}
             onChange={(e) => setTitle(e.target.value)}
           />
-        </div>
+        </div> */}
         <div className="writeFormGroup">
           <textarea
             placeholder="Tell your story..."
@@ -68,6 +98,16 @@ function Write() {
             className="writeInput writeText"
             onChange={(e) => setDesc(e.target.value)}
           ></textarea>
+        </div>
+        <div className="writeFormGroup">
+          <label htmlFor="categoryInput">Category</label>
+          <input
+            type="text"
+            id="categoryInput"
+            placeholder="Enter category"
+            className="writeInput"
+            onChange={(e) => setCategory(e.target.value)}
+          />
         </div>
         <button className="writeSubmit" type="submit">
           Publish
